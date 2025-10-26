9250c8477fc9d7fb79f5f3747fa66670d7121add
juanifabrega <juanifabrega1997@gmail.com>
2025-10-25 21:27:05 -0300

    structure:contentcomponent

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { SkillsComponent } from "../skills/skills.component";
+import { ProfileComponent } from '../profile/profile.component';
+import { ExperienceComponent } from '../experience/experience.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { EducationComponent } from '../education/education.component';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [ProfileComponent,ExperienceComponent,ProjectsComponent,SkillsComponent,EducationComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
